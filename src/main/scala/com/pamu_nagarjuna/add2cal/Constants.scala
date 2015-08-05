4a2799c8b8100e0676077be2456aa7e9adb95982
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-08-05 10:45:17 +0530

    EmailNotifier added

---
+++ src/main/scala/com/pamu_nagarjuna/add2cal/Constants.scala
@@ -0,0 +1,12 @@
+package com.pamu_nagarjuna.add2cal
+
+/**
+ * Created by pnagarjuna on 04/08/15.
+ */
+object Constants {
+  val HOST = ""
+  val USER = ""
+  val PASSWORD = ""
+  val BOX = ""
+  val FREQ = ""
+}
